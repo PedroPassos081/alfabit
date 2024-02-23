ef1bb1653ab1edee70c3521c047bd70b8532d04a
PedroPassos081 <pedro.passos081@gmail.com>
2024-02-23 16:57:01 -0300

    Criando as variaveis no tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,37 @@
-import type { Config } from "tailwindcss";
+function toRgba(cssVariable) {
+  const color = `var(${cssVariable})`
+  return ({ opacityValue }) => `rgba(${color}, ${opacityValue})`
+}
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      
+      colors: {
+        primary: toRgba('--primary'),
+        secondary: toRgba('--secondary'),
+        tertiary: toRgba('--tertiary'),
+        quaternary: toRgba('--quaternary'),
+        hover: toRgba('--hover'),
+        click: toRgba('--click'),
+        icon: toRgba('--icon'),
+        outline: toRgba('--outline'),
+        divider: toRgba('--divider'),
+        'bg-light': toRgba('--bg-light'),
+        'bg-dark': toRgba('--bg-dark'),
+        'bg-disabled': toRgba('--bg-disabled'),
+        'text-primary': toRgba('--text-primary'),
+        'text-secondary': toRgba('--text-secondary'),
+        'text-tertiary': toRgba('--text-tertiary'),
+        'text-disabled': toRgba('--text-disabled'),
+      },
     },
+
   },
   plugins: [],
-};
-export default config;
+}
