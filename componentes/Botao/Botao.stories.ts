53e845f34b1d91e09f8a71615a93ad57f99f5660
Pedro Passos <pedro.passos081@gmail.com>
2024-03-06 22:51:28 -0300

    Finalizando o botao

--- componentes/Botao/Botao.stories.ts
+++ componentes/Botao/Botao.stories.ts
@@ -1,11 +1,22 @@
 import { Meta, StoryObj } from "@storybook/react";
-import Botao, { BotaoProps } from ".";
+import Botao, { BotaoProps } from "./index"
 
 
 const meta: Meta<BotaoProps> = {
     title: 'Molecules/Button',
     component: Botao,
-    argTypes: {}
+    argTypes: {
+        children: {
+            type: "string"
+        },
+        disabled: {
+            type: "boolean"
+        },
+        className: {
+            type: "string"
+        },
+    }
+
 }
 
 export default meta
@@ -16,9 +27,55 @@ export const Primary: StoryObj<BotaoProps> = {
     }
 }
 
+export const Disabled: StoryObj<BotaoProps> = {
+    args: {
+        children: 'Botão',
+        disabled: true
+    }
+}
+
+export const Secondary: StoryObj<BotaoProps> = {
+    args: {
+        children: 'Botão',
+        variant: 'secondary'
+    }
+}
+
+export const Tertiary: StoryObj<BotaoProps> = {
+    args: {
+        children: 'Botão',
+        variant: 'tertiary'
+    }
+}
+
+export const TertiaryDisabled: StoryObj<BotaoProps> = {
+    args: {
+        children: 'Botão',
+        variant: 'tertiary',
+        disabled: true
+    }
+}
+
+
 export const Violet: StoryObj<BotaoProps> = {
     args: {
         children: 'Botão',
         className: 'theme-violet'
+    }
+}
+
+export const VioletSecondary: StoryObj<BotaoProps> = {
+    args: {
+        children: 'Botão',
+        className: 'theme-violet',
+        variant: 'secondary'
+    }
+}
+
+export const VioletTertiary: StoryObj<BotaoProps> = {
+    args: {
+        children: 'Botão',
+        className: 'theme-violet',
+        variant: 'tertiary'
     }
 }
--- componentes/Botao/index.tsx
+++ componentes/Botao/index.tsx
@@ -1,16 +1,28 @@
 export type BotaoProps = {
   children: React.ReactNode;
+  variant?: 'primary' | 'secondary' | 'tertiary';
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Botao = ({ children, className }: BotaoProps) => {
-  return <button
-    className={`
-      bg-primary rounded-md px-6 py-2 text-white
-      ${className}
-    `}
-  >
-    { children }
-  </button>
+function getVariant(variant: BotaoProps['variant'], disabled: BotaoProps['disabled']) {
+  switch (variant) {
+    case 'primary':
+      return disabled ? 'bg-disabled text-disabled' : 'bg-primary text-white'
+    case 'secondary':
+      return disabled ? 'bg-disabled text-disabled' : 'bg-quaternary text-primary'
+    case 'tertiary':
+    default:
+      return disabled ? 'text-disabled' : 'text-primary'
+  }
 }
 
-export default Botao
+const Botao = ({ variant = 'primary', children, className, disabled, ...rest }: BotaoProps) => {
+  return <Botao
+    className={`rounded-md px-6 py-2 ${getVariant(variant, disabled)} ${className}`}
+    disabled={disabled}
+    {...rest}
+  >
+    { children }
+  </Botao>
+}
+
+export default <Botao></Botao>
