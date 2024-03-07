465d1985c15e087d567cf54d92cc2518056025ee
Pedro Passos <pedro.passos081@gmail.com>
2024-03-06 22:20:23 -0300

    alterando o arquivo

--- componentes/Botao/index.tsx
+++ componentes/Botao/index.tsx
@@ -1,16 +1,16 @@
 export type BotaoProps = {
-    children: React.ReactNode;
-  } & React.ButtonHTMLAttributes<HTMLButtonElement>;
-  
-  const Botao = ({ children, className }: BotaoProps) => {
-    return <button
-      className={`
-        bg-primary rounded-md px-6 py-2 text-white
-        ${className}
-      `}
-    >
-      { children }
-    </button>
-  }
+  children: React.ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Botao = ({ children, className }: BotaoProps) => {
+  return <button
+    className={`
+      bg-primary rounded-md px-6 py-2 text-white
+      ${className}
+    `}
+  >
+    { children }
+  </button>
+}
 
 export default Botao
